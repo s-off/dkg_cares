58d9d5b24a7958b6f006d304cce7c09601a0cc4e
s-off <89302140+s-off@users.noreply.github.com>
2022-02-27 03:02:08 +0100

    Fixing Error in the inclusion/exclusion citeria and adapting table 1 to new dataset.

--- Syntax/DKG_CARES_Syntax.R
+++ Syntax/DKG_CARES_Syntax.R
@@ -4,11 +4,9 @@
 
 # install.packages("haven")
 # install.packages("tidyverse")
-# install.packages("stargazer")
-# install.packages("ggplot2")
-# install.packages("knitr")
 # install.packages("gt")
 # install.packages("gtsummary")
+# install.packages("labelled")
 
 library(haven)
 library(tidyverse)
@@ -38,17 +36,20 @@ cancer_CASES_MCB_KOB <- MCB %>%
   mutate(RehaStart = mcbemsj*12 + mcbemsm) %>%          # Erste Episode wird durch RehaStart (Beginn der medizinischen Reha-Leistung) identifiziert
   arrange(case, RehaStart) %>% 
   distinct(case, .keep_all = TRUE) %>%
-  inner_join(KOB, by = "case") %>%                      # Zusammenführung von MCB- und KOB-Daten
+  inner_join(KOB, by = "case") %>%                      # Zusammenführen von MCB- und KOB-Daten
   mutate(GBZeitpkt = gbja*12 + gbmo,                    # Alter in Monaten
          RTZeitpkt = rtbej*12 + rtbem,                  # Rentenbeginn in Monaten
          TDZeitpkt = tddtj*12 + tddtm,                  # Todeszeitpunkt in Monaten
          AlterRehaStart = RehaStart - GBZeitpkt,        # Alter in Monaten bei Beginn der ersten med. Reha
+         ZeitRehaStartRente = case_when(                # Zeitraum zwischen Beginn der ersten med. Reha und dem Rentenbeginn in Monaten
+           rtbej %in% c(1:9998) ~ RTZeitpkt - RehaStart),
          ZeitRehaStartTod = case_when(                  # Zeitraum zwischen Beginn der ersten med. Reha und dem Todeszeitpunkt in Monaten
-           tddtj %in% c(1:9998) ~ TDZeitpkt - RehaStart)) %>%
-  filter(RTZeitpkt < RehaStart,                         # Keine Rente vor Beginn der ersten med. Reha
+           tddtj %in% c(1:9998) ~ TDZeitpkt - RehaStart),
+         RTvorReha = if_else(
+           ZeitRehaStartRente < 0, 1, 0)) %>%
+  filter(!RTvorReha %in% c(1),                          # Keine Rente vor Beginn der ersten med. Reha
          AlterRehaStart %in% c(216:804),                # Alter zwischen 18 und 67
          tddtj != 1992)                                 # Ausschluss Artefakt mit Todesjahr 1992
-
 
 
 
@@ -61,8 +62,6 @@ CARES_rtw <- cancer_CASES_MCB_KOB %>%
       sex == 2 ~ "weiblich"),
     AlterRehaStart_Jahr = case_when(
       gbja > 0 ~ mcbemsj-gbja),
-    Rente = if_else(
-      rtbej > 0, 1, 0),
     Tod = if_else(
       tddtj > 0, 1, 0)
     ) %>%
@@ -74,10 +73,11 @@ CARES_rtw <- cancer_CASES_MCB_KOB %>%
 
 CARES_rtw %>%
   select(Geschlecht,
-         mcdg1_icd,
          AlterRehaStart_Jahr,
          mcfmsd,
-         Rente,
+         mcdg1_icd,
+         tlrt,
+         ZeitRehaStartRente,
          Tod,
          ZeitRehaStartTod,
          mcaiufzt,
@@ -92,20 +92,21 @@ CARES_rtw %>%
          mcwhot_ostwest,
          mcwhot_skt,
          mcmsot_bland) %>% 
-  mutate_at(vars("bd", "sb", "bb", "mcahb", "mcfmsd", "mcwhot_ostwest", "mcwhot_skt", "mcmsot_bland", "mcstbf", "mcaiufzt", "mcleft_lb", "mcleft_at", "mcdg1_icd"), haven::as_factor) %>%
+  mutate_at(vars("bd", "sb", "bb", "mcahb", "mcfmsd", "mcwhot_ostwest", "mcwhot_skt", "mcmsot_bland", "mcstbf", "mcaiufzt", "mcleft_lb", "mcleft_at", "mcdg1_icd", "tlrt"), haven::as_factor) %>%
   tbl_summary(
     by = Geschlecht,
-    missing_text = "Fehlende Beobachtungen",
     statistic = all_continuous() ~ "{mean} ({sd})",
     label = list(
       AlterRehaStart_Jahr ~ "Alter bei Beginn der ersten Rehabilitationsleistung",
-      Rente ~ "Versichertenrente",
+      mcdg1_icd ~ "Medizinische Entlassungsdiagnose der Rehabilitation",
+      ZeitRehaStartRente ~ "Zeitraum vom Beginn der ersten medizinischen Rehabilitation bis zum Rentenbeginn in Monaten",
       Tod ~ "Versterben im Beobachtungszeitraum",
-      ZeitRehaStartTod ~ "Zeitraum vom Beginn der ersten med. Reha und dem Todeszeitpunkt (Monate)",
+      ZeitRehaStartTod ~ "Zeitraum vom Beginn der ersten medizinischen Rehabilitation bis zum Todeszeitpunkt in Monaten",
       mcaqda ~ "Zeitraum von der Antragstellung eines Antrags auf medizinische Rehabilitation bis zu seiner Bewilligung in Tagen"),
     digits = list(
       AlterRehaStart_Jahr ~ 1,
-      ZeitRehaStartTod ~ 0)) %>% 
+      ZeitRehaStartTod ~ 0),
+    missing = "no") %>% 
   add_overall() %>% 
   modify_header(label = "**Variable**") %>% 
   bold_labels()
@@ -114,80 +115,12 @@ CARES_rtw %>%
 
 
 
-##### Count #####
+
+##### Experimental #####
 
 
 x <- CARES_rtw %>% 
-  group_by(mcdg1_icd) %>% 
+  group_by(breha_3) %>% 
   summarise(count = n())
 
 view(x)
-
-
-
-
-x <- CARES_rtw %>% 
-  group_by(mcdg1_icd) %>% 
-  summarise(count = n())
-
-view(x)
-
-
-
-
-
-
-
-
-### evtl. noch nützlich
-
-## Geburtsjahr Plot
-# Geburtsjahr <- cancer_CASES_MCB_KOB %>%
-#   filter(gbja>0)
-# 
-# hist(Geburtsjahr$gbja, breaks=100)
-
-
-
-# dataset with few variables to summarize
-x1 <- final_CARES %>% 
-  select(AlterRehaStart, Geschlecht)
-
-table1 <- tbl_summary(x1)
-
-
-
-# simple count
-CARES_rtw %>% 
-  count(mcaift)
-
-# simple mean
-final_CARES %>%
-  group_by(GroupeVariable) %>%
-  summarize(Mean = mean(MeanVariable, na.rm=TRUE))
-
-
-
-# () tlrt
-x <- final_CARES %>%
-  group_by(tlrt) %>% 
-  summarise(mean = round(mean(AlterRehaStart, na.rm=TRUE), 2),
-            min = min(AlterRehaStart, na.rm=TRUE),
-            max = max(AlterRehaStart, na.rm=TRUE),
-            count = n())
-
-
-##### Notes #####
-
-## Rehabeginn abhängig vom Monat. Um Neujahr weniger Rehaantritte (Teilweise bis zu 50 %).
-## breaks=96 weil 8 Jahre = 96 Monate
-# hist(cancer_CASES_MCB$RehaStart, breaks=96)
-# 
-# RehaBeginnMonat <- cancer_CASES_MCB_KOB %>% 
-#   count(RehaStart, mcbemsm, wt=korrektur)
-# 
-# ggplot(RehaBeginnMonat, aes( RehaStart, n, colour = mcbemsm) ) +
-#   geom_point( ) + geom_line( )
-
-
-
